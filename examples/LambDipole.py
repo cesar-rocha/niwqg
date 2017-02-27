e3dc7dbbed4ad90c6836c25e473502b23e4ba674
Cesar B. Rocha <crocha@ucsd.edu>
2017-02-27 14:06:37 -0800

    Adds examples

---
+++ examples/LambDipole.py
@@ -0,0 +1,124 @@
+"""
+ Initially laterally coherent near-inertial oscillation
+    coupled with Lamb dipole.
+
+ This example runs in about 1.5 min on a MacPro 2.2 GHz Intel Core i7,
+    16 GB 1600 MHz DDR3.
+"""
+import timeit
+start = timeit.default_timer()
+
+import matplotlib.pyplot as plt
+plt.rcParams['contour.negative_linestyle'] = 'dashed'
+import numpy as np
+
+from niwqg import UnCoupledModel as Model
+from niwqg import InitialConditions as ic
+
+plt.close('all')
+
+# parameters
+nx = 128
+f0 = 1.e-4
+N = 0.01
+L = 2*np.pi*200e3
+λz = 280
+m = 2*np.pi/λz
+
+# eddy parameters
+k0 = 10*(2*np.pi/L)
+L0 = 2*np.pi/k0
+
+# initial conditions
+U = 1.e-1
+phi0 = 2*U
+U0 = U
+u0 = phi0
+
+# simulation parameters
+Te = (U0*k0)**-1 # eddy turn-over time scale
+Tf = 2*np.pi/f0
+
+dt = .025*Te
+tmax = 10*Te
+
+m = Model.Model(L=L,nx=nx, tmax = tmax,dt = dt,
+                m=m,N=N,f=f0, twrite=int(0.25*Tf/dt),
+                nu4=8.e8,nu4w=8.e8,use_filter=False,
+                U =-U, tdiags=2,)
+#nu4=7.5e8,nu4w=7.5e8,use_filter=False,
+
+# initial conditions
+q = ic.LambDipole(m, U=U,R = 2*np.pi/k0)
+phi = (np.ones_like(q) + 1j)*u0/np.sqrt(2)
+
+m.set_q(q)
+m.set_phi(phi)
+
+# run the model
+m.run()
+
+# get diagnostics
+time = m.diagnostics['time']['value']
+KE_qg = m.diagnostics['ke_qg']['value']
+PE_niw = m.diagnostics['pe_niw']['value']
+KE_niw = m.diagnostics['ke_niw']['value']
+ENS_qg = m.diagnostics['ens']['value']
+
+g1 = m.diagnostics['gamma_r']['value']
+g2 = m.diagnostics['gamma_a']['value']
+pi = m.diagnostics['pi']['value']
+cKE_niw = m.diagnostics['cke_niw']['value']
+iKE_niw = m.diagnostics['ike_niw']['value']
+
+ep_phi = m.diagnostics['ep_phi']['value']
+ep_psi = m.diagnostics['ep_psi']['value']
+chi_q =  m.diagnostics['chi_q']['value']
+chi_phi =  m.diagnostics['chi_phi']['value']
+
+dt = time[1]-time[0]
+dPE = np.gradient(PE_niw,dt)
+dKE = np.gradient(KE_qg,dt)
+diKE_niw = np.gradient(iKE_niw,dt)
+
+fig = plt.figure(figsize=(16,9))
+lw, alp = 3.,.5
+KE0 = KE_qg[0]
+
+ax = fig.add_subplot(221)
+plt.plot(time/Te,KE_qg/KE0,label='KE QG',linewidth=lw,alpha=alp)
+plt.plot(time/Te,KE_niw/KE_niw[0],label='KE NIW',linewidth=lw,alpha=alp)
+plt.plot(time/Te,ENS_qg/ENS_qg[0],label='ENS QG',linewidth=lw,alpha=alp)
+plt.xticks([])
+plt.ylabel(r'Energy/Enstrophy $[E/E_0, Z/Z_0]$')
+plt.legend(loc=3)
+
+ax = fig.add_subplot(222)
+plt.plot(time/Te,(KE_qg-KE_qg[0])/KE0,label='KE QG',linewidth=lw,alpha=alp)
+plt.plot(time/Te,(PE_niw-PE_niw[0])/KE0,label='PE NIW',linewidth=lw,alpha=alp)
+plt.plot(time/Te,(KE_niw-KE_niw[0])/KE0,label='KE NIW',linewidth=lw,alpha=alp)
+plt.xticks([])
+plt.ylabel(r'Energy  change $[(E-E_0) \times {2}/{U_0^2} ]$')
+plt.legend(loc=3)
+
+ax = fig.add_subplot(223)
+plt.plot(time/Te,Te*g1/KE0,label=r'Refraction conversion $-\Gamma_1$',linewidth=lw,alpha=alp)
+plt.plot(time/Te,Te*g2/KE0,label=r'Advection conversion $-\Gamma_2$',linewidth=lw,alpha=alp)
+plt.plot(time/Te,-Te*chi_q/KE0,label=r'KE QG dissipation $-\epsilon_\psi$',linewidth=lw,alpha=alp)
+plt.plot(time/Te,Te*(g1+g2-chi_phi)/KE0,label=r'$-(\Gamma_1+\Gamma_2+\epsilon_\psi)$',linewidth=lw,alpha=alp)
+plt.plot(time/Te,-Te*dPE/KE0,'k--',label=r'KE QG tendency $\dot K_e$',linewidth=lw,alpha=alp)
+plt.legend(loc=2)
+plt.xlabel(r"Time [$t \times U_0 k_0$]")
+plt.ylabel(r'Power $[\dot E \times {2 k_0}/{U_0} ]$')
+
+ax = fig.add_subplot(224)
+plt.plot(time/Te,Te*pi/KE0,label=r'Incoherent KE NIW conversion $\Pi$',linewidth=lw,alpha=alp)
+plt.plot(time/Te,-Te*ep_phi/KE0,label=r'KE NIW disspation $-\epsilon_\phi$',linewidth=lw,alpha=alp)
+plt.plot(time/Te,Te*(pi-ep_phi)/KE0,label=r'$\pi-\epsilon_\phi$',linewidth=lw,alpha=alp)
+plt.plot(time/Te,Te*diKE_niw/KE0,'k--',label=r'Incoherent NIW KE tendency',linewidth=lw,alpha=alp)
+plt.xlabel(r"Time [$t \times U_0 k_0$]")
+plt.ylabel(r'Power $[\dot E \times {2 k_0}/{U_0} ]$')
+plt.legend(loc=3)
+
+stop = timeit.default_timer()
+print("Time elapsed: %3.2f seconds" %(stop - start))
---
+++ examples/LambDipole_qg.py
@@ -0,0 +1,80 @@
+"""
+    Barotropic, f-plane solution with initial condition
+        given by the Lamb dipole.
+
+ This example runs in about 3 sec on a MacPro 2.2 GHz Intel Core i7,
+    16 GB 1600 MHz DDR3.
+"""
+import timeit
+start = timeit.default_timer()
+
+import matplotlib.pyplot as plt
+plt.rcParams['contour.negative_linestyle'] = 'dashed'
+import numpy as np
+
+#from CoupledModel import Model
+from niwqg import QGModel as Model
+from niwqg import InitialConditions as ic
+
+plt.close('all')
+
+# parameters
+nx = 128
+L = 2*np.pi*200e3
+
+# eddy parameters
+k0 = 10*(2*np.pi/L)
+L0 = 2*np.pi/k0
+
+# initial conditions
+U = 1.e-1
+
+# simulation parameters
+Te = (U*k0)**-1
+
+dt = .05*Te
+tmax = 10*Te
+
+path = "128/lamb/moderate_filter"
+#path = "512/lamb/large_amp"
+m = Model.QGModel(L=L,nx=nx, tmax = tmax,dt = dt, twrite=int(0.1*Te/dt),
+                    nu4=7.5e8, use_filter=False,save_to_disk=False,
+                    tsave_snapshots=5,path=path,
+                    U =-U, tdiags=1, beta = 0.)
+
+#q = McWilliams1984(m,k0=k0,E=U0**2/2)
+q = ic.LambDipole(m, U=U,R = 2*np.pi/k0)
+
+m.set_q(q)
+
+m._invert()
+
+# plot initial condition
+xqmin, yqmin = [],[]
+xqmax, yqmax = [],[]
+R = 2*np.pi/k0
+r = k0*L/2/np.pi
+
+# run the model
+m.run()
+
+time = m.diagnostics['time']['value']
+KE_qg = m.diagnostics['ke_qg']['value']
+ENS_qg = m.diagnostics['ens']['value']
+ep_psi = m.diagnostics['ep_psi']['value']
+chi_q =  m.diagnostics['chi_q']['value']
+dt = time[1]-time[0]
+dKE = np.gradient(KE_qg,dt)
+
+plt.figure(figsize=(12,6))
+lw, alp = 3.,.5
+plt.plot(time/Te,-Te*ep_psi/KE_qg[0], label=r'KE dissipation $-\epsilon_\psi$',
+            linewidth=lw,alpha=alp)
+plt.plot(time/Te,Te*dKE/KE_qg[0],'k--',label=r'KE tendency $\dot K_e$',
+            linewidth=lw,alpha=alp)
+plt.xlabel(r"Time [$t \times U_0 k_0$]")
+plt.ylabel(r'Power $[\dot E \times {2 k_0}/{U_0} ]$')
+plt.legend(loc=4)
+
+stop = timeit.default_timer()
+print("Time elapsed: %3.2f seconds" %(stop - start))
